0c6cb765b486291130f02a25715b25d9a833b629
jiading555 <yangjd-wm24@student.tarc.edu.my>
2025-05-02 22:58:35 +0800

    Update Staff.java

--- Ordering System complete/Ordering System (2)/Ordering System/Ordering System/src/Staff.java
+++ Ordering System complete/Ordering System (2)/Ordering System/Ordering System/src/Staff.java
@@ -3,16 +3,6 @@
     import java.util.List;
     import java.util.Scanner;
 
-
-    /*
-     * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-     * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-     */
-
-    /**
-     *
-     * @author user 
-     */
     public class Staff {
         private static List<Product> products = new ArrayList<>();
        
