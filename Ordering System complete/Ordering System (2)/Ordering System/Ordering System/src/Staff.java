3eb2d42e4624a81c038fb6ab122b4df6f957e3fa
jiading555 <yangjd-wm24@student.tarc.edu.my>
2025-05-02 22:58:22 +0800

    Update Staff.java

--- Ordering System complete/Ordering System (2)/Ordering System/Ordering System/src/Staff.java
+++ Ordering System complete/Ordering System (2)/Ordering System/Ordering System/src/Staff.java
@@ -1,12 +1,7 @@
-import java.io.BufferedReader;
-    import java.io.FileReader;
-    import java.io.FileWriter;
-    import java.io.IOException;
-    import java.io.PrintWriter;
     import java.util.ArrayList;
     import java.util.Comparator;
     import java.util.List;
-import java.util.Scanner;
+    import java.util.Scanner;
 
 
     /*
